86e3bc347efbef1f32bb6d4709cb183a0f3eeb58
rgoncar723 <rgoncar723@g.edcuaand.es>
2026-03-11 08:53:15 +0100

    Atributos y metodos de Consola e IEntradaSalida

--- src/rgoncar723/Consola.java
+++ src/rgoncar723/Consola.java
@@ -1,5 +1,94 @@
 package rgoncar723;
 
-public class Consola {
+import java.util.Scanner;
+
+public class Consola implements IEntradaSalida {
+	//Atributo
+	private Scanner sc;
+	
+	
+	//Constructor
+	public Consola() {
+		this.sc = new Scanner(System.in);
+	}
+	@Override
+	public void imprimir(String texto) {
+		System.out.println(texto);
+		
+	}
+
+	@Override
+	public void imprimirLinea(String texto) {
+		System.out.print(texto);
+		
+	}
+
+	@Override
+	public String leerTexto(String mensaje) {
+		imprimir(mensaje);
+		mensaje = sc.nextLine();
+		return mensaje.trim();
+	}
+
+	@Override
+	public int leerEntero(String mensaje) {
+		boolean error = false;
+		int numero = 0;
+		do {
+			error = false;
+			String texto = leerTexto(mensaje);
+			try {
+				numero = Integer.parseInt(texto);
+			} catch (NumberFormatException e) {
+				imprimirLinea("Error: Introduce un número entero válido.");
+				error = true;
+			}
+
+		} while (error);
+		return numero;
+	}
+
+	@Override
+	public double leerImporte(String mensaje) {
+		boolean error = false;
+		double numero = 0.0;
+		do {
+			error = false;
+			String texto = leerTexto(mensaje);
+			try {
+				numero = Integer.parseInt(texto);
+			} catch (NumberFormatException e) {
+				imprimirLinea("Error: Introduce un número válido.");
+				error = true;
+			}
+
+		} while (error);
+		return numero;
+	}
+
+	@Override
+	public void mostrarMenu() {
+		imprimirLinea("====Menu====");
+		imprimirLinea("1. Contratar a un empleado");
+		imprimirLinea("2. Listar empleados");
+		imprimirLinea("3. Aplicar filtros");
+		imprimirLinea("4. Salir del programa");
+		imprimirLinea("============");
+		
+	}
+
+	@Override
+	public void limpiarPantalla() {
+		for(int i=0; i<=50;i++) {
+			imprimir(" ");
+		}	
+	}
+
+	@Override
+	public void pausa() {
+		imprimir("Pulse una tecla para continuar...");
+		sc.nextLine();
+		
+	}
 
 }
--- src/rgoncar723/IEntradaSalida.java
+++ src/rgoncar723/IEntradaSalida.java
@@ -1,5 +1,12 @@
 package rgoncar723;
 
 public interface IEntradaSalida {
-
+	void imprimir(String texto);
+	void imprimirLinea(String texto);
+	String leerTexto (String mensaje);
+	int leerEntero (String mensaje);
+	double leerImporte(String mensaje);
+	void mostrarMenu();
+	void limpiarPantalla();
+	void pausa();
 }
