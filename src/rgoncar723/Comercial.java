c6227fa9eef5ecc44e5f9ed71d38821717681db6
rgoncar723 <rgoncar723@g.edcuaand.es>
2026-03-11 09:28:28 +0100

    Atributos y metodos de Plantilla

--- src/rgoncar723/Comercial.java
+++ src/rgoncar723/Comercial.java
@@ -4,9 +4,8 @@ public class Comercial extends Empleado {
 	//Atributo
 	private double ventas;
 	//Constructor
-	public Comercial(String dni, String nombre, String apellidos, double sueldoBase,double ventas) {
+	public Comercial(String dni, String nombre, String apellidos, double sueldoBase) {
 		super(dni, nombre, apellidos, sueldoBase);
-		this.ventas=ventas;
 	}
 	//Metodos
 	void setVentas(double ventas) {
--- src/rgoncar723/Plantilla.java
+++ src/rgoncar723/Plantilla.java
@@ -1,5 +1,29 @@
 package rgoncar723;
 
-public class Plantilla {
+import java.util.*;
 
+public class Plantilla {
+	private List<Empleado> listaEmpleados = new ArrayList<>();
+
+	public void contratarEmpleado(Empleado empleado) {
+		listaEmpleados.add(empleado); //añade el empleado a lista 
+	}
+
+	public List<Empleado> getEmpleadosPorNombre(String filtroNombre) {
+		List<Empleado> filtrados = new ArrayList<>();
+		String noFiltros = filtroNombre.toLowerCase();
+		if (filtroNombre == null || filtroNombre.isEmpty()) {
+			return new ArrayList<>(listaEmpleados);
+		}
+
+		for (Empleado e : listaEmpleados) {
+			if (e.getNombre().toLowerCase().contains(noFiltros) // Obtiene el nombre, lo pasa a minusculas y lo compara
+																// con noFiltro para comprobar si son iguales.
+					|| (e.getApellidos().toLowerCase().contains(noFiltros))) {
+				filtrados.add(e);
+			}
+		}
+		return filtrados;
+
+	}
 }
